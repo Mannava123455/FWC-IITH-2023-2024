554bf0df03e11db4cdd60c5f6b5d4d2dd881a936
Mannava Venkatasai <111038679+Mannava123455@users.noreply.github.com>
2022-08-28 13:58:52 +0530

    Update main.cpp

--- FWC_assignment_1_Mannava_Venkatasai/codes/src/main.cpp
+++ FWC_assignment_1_Mannava_Venkatasai/codes/src/main.cpp
@@ -1,13 +1,6 @@
 #include<Arduino.h>
 int x,y,z,w;
-int f,A,B,C,D;
-void disp_7447(int D, int C, int B, int A)
-{
-  digitalWrite(6, A); //LSB
-  digitalWrite(7, B); 
-  digitalWrite(8, C); 
-  digitalWrite(9, D); //MSB
-  }
+int f;
 void setup()
 {
 pinMode(2,INPUT);
@@ -24,8 +17,7 @@ z=digitalRead(4);
 w=digitalRead(5);
 f=(!x||y)&&z|| !w;
 digitalWrite(13,f);
-A=f;
-disp_7447(D,C,B,A);
 }
+// OUTput pin 13 of arduino is given to the 7 th pin of 7447 IC and the remaining  1,2,6 pins are grounded.It results in display of either 0 or 1 in seven segment display. 
 
 
