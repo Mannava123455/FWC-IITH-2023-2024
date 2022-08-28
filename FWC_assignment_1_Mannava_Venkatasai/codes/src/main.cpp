24ef3ec9f596691ef04ee377eddb7dc0cdb5b365
Mannava Venkatasai <111038679+Mannava123455@users.noreply.github.com>
2022-08-28 13:59:48 +0530

    Update main.cpp

--- FWC_assignment_1_Mannava_Venkatasai/codes/src/main.cpp
+++ FWC_assignment_1_Mannava_Venkatasai/codes/src/main.cpp
@@ -15,7 +15,7 @@ x=digitalRead(2);
 y=digitalRead(3);
 z=digitalRead(4);
 w=digitalRead(5);
-f=(!x||y)&&z|| !w;
+f=((!x||y)&&z)||!w;
 digitalWrite(13,f);
 }
 // OUTput pin 13 of arduino is given to the 7 th pin of 7447 IC and the remaining  1,2,6 pins are grounded.It results in display of either 0 or 1 in seven segment display. 
