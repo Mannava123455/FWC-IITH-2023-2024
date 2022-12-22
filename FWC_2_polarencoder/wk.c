68eff9fb97ff32335916cc07b5bfb2a7cf8e4c3a
Mannava Venkatasai <111038679+Mannava123455@users.noreply.github.com>
2022-12-22 10:29:49 +0530

    Update wk.c
    polar encoder without matrix multiplication

--- FWC_2_polarencoder/wk.c
+++ FWC_2_polarencoder/wk.c
@@ -93,15 +93,6 @@ for(i=0;i<N;i++)
 }
 
 
-/*m = 1; %number of bits combined
-for d = n-1:-1:0
-    for i = 1:2*m:N
-        a = u(i:i+m-1); %first part
-        b = u(i+m:i+2*m-1); %second part
-        u(i:i+2*m-1) = [mod(a+b,2) b]; %combining
-    end
-    m = m * 2; 
-end*/
 
 int m1=1;
 int d,a[N],b[N],ress[N],conc[N];
